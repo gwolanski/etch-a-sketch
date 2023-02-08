87fae618ac23f590f36790d43b8cf7837ad586c2
gwolanski <gwol@udel.edu>
2023-02-08 13:36:35 -0700

    Added reset button and size slider

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let divContainer = document.getElementById('container');
+let resetButton = document.getElementById("reset");
 
-//function that generates 256 empty divs with class 'sketch'
-function generateDivs() {
-    let numberOfDivs = 256
-    
-    for (let i = 0; i < numberOfDivs; i++) {
+gridLength = 16;
+
+let slider = document.getElementById("myRange");
+let sliderOutput = document.getElementById("size");
+sliderOutput.innerHTML = slider.value;
+
+slider.oninput = function() {
+    sliderOutput.innerHTML = this.value;
+}
+
+//function that generates 256 empty divs with class 'gridItem'
+function createGrid(gridLength) {
+    let gridSize = gridLength * gridLength;
+
+    divContainer.style.gridTemplateRows = `repeat(${gridLength}, 40px)`;
+    divContainer.style.gridTemplateColumns = `repeat(${gridLength}, 40px)`;
+
+    for (let i = 0; i < gridSize; i++) {
         let squareDivs = document.createElement('div');
         squareDivs.className = "gridItem";
         divContainer.appendChild(squareDivs);
+
+    //create function that will clear/reset the grid
+        function clearGrid() {
+            resetButton.addEventListener('click', e => {
+                squareDivs.style.backgroundColor = "white";
+            })
+        }
+
+        clearGrid();
     }
 }
 
-generateDivs();
+createGrid();
+
+let selectedDiv = document.querySelector("div");
+selectedDiv.addEventListener("mouseover", function(event) {
+    event.target.classList.replace("gridItem", "hover");
+});
