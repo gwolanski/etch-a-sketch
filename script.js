4835aa8649560ffbf29906ed82a335787c714c9d
gwolanski <gwol@udel.edu>
2023-02-11 09:24:13 -0700

    Grid will now update depending on slider value

--- script.js
+++ script.js
@@ -1,42 +1,96 @@
-let divContainer = document.getElementById('container');
+const divContainer = document.querySelector("#gridContainer");
 let resetButton = document.getElementById("reset");
 
-gridLength = 16;
+let gridLength = 16;
 
 let slider = document.getElementById("myRange");
+let sliderValue = document.getElementById("myRange").value;
 let sliderOutput = document.getElementById("size");
-sliderOutput.innerHTML = slider.value;
+sliderOutput.innerHTML = `${slider.value} x ${slider.value}`;
 
 slider.oninput = function() {
     sliderOutput.innerHTML = this.value;
 }
 
-//function that generates 256 empty divs with class 'gridItem'
-function createGrid(gridLength) {
-    let gridSize = gridLength * gridLength;
 
-    divContainer.style.gridTemplateRows = `repeat(${gridLength}, 40px)`;
-    divContainer.style.gridTemplateColumns = `repeat(${gridLength}, 40px)`;
+
+
+
+//function that generates 256 empty divs with class 'gridItem'
+function createGrid(size) {
+    
+    divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    divContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let gridSize = size * size;
 
     for (let i = 0; i < gridSize; i++) {
-        let squareDivs = document.createElement('div');
+        let squareDivs = document.createElement("div");
         squareDivs.className = "gridItem";
         divContainer.appendChild(squareDivs);
-
-    //create function that will clear/reset the grid
-        function clearGrid() {
-            resetButton.addEventListener('click', e => {
-                squareDivs.style.backgroundColor = "white";
-            })
-        }
-
-        clearGrid();
     }
 }
 
-createGrid();
+function reloadGrid(size) {
+    divContainer.innerHTML = "";
+    createGrid(size);
+}
 
-let selectedDiv = document.querySelector("div");
-selectedDiv.addEventListener("mouseover", function(event) {
-    event.target.classList.replace("gridItem", "hover");
-});
+
+function changeGrid(size) {
+    gridLength = size;
+    reloadGrid(gridLength);
+}
+
+createGrid(16);
+
+slider.addEventListener("change", (e) => changeGrid(slider.value));
+
+
+ //   let newSliderValue = document.getElementById("myRange").value;
+  //  divContainer.innerHTML = "";
+ //   divContainer.style.gridTemplateColumns = `repeat(${newSliderValue}, 1fr)`;
+ //   divContainer.style.gridTemplateRows = `repeat(${newSliderValue}, 1fr)`;
+    
+    
+ //   let gridSize = newSliderValue * newSliderValue;
+ //   console.log(newSliderValue);
+
+  //  for (let i = 0; i < gridSize; i++) {
+ //       const squareDivs = document.createElement('div');
+  //      squareDivs.className = "gridItem";
+ //      divContainer.appendChild(squareDivs);
+
+ //   console.log(sliderValue);
+ //   };
+
+
+// sliderValue.addEventListener("change", updateGrid());
+
+
+    //take slider value and assign the value to gridLength
+    
+    
+    //this for loop should be good and shouldnt need editing
+ //   for (let i = 0; i < gridSize; i++) {
+  //      let squareDivs = document.createElement('div');
+  //      squareDivs.className = "gridItem";
+  //      divContainer.appendChild(squareDivs);
+
+    //create function that will clear/reset the grid
+   //     function clearGrid() {
+   //         resetButton.addEventListener('click', e => {
+   //             squareDivs.style.backgroundColor = "white";
+   //         })
+    //    }
+
+   //     clearGrid();
+   // }
+//}
+
+
+
+// let selectedDiv = document.querySelector("div");
+// selectedDiv.addEventListener("mouseover", function(event) {
+ //   event.target.classList.replace("gridItem", "hover");
+// });
