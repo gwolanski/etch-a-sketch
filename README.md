30191dc91df2d05b6f5f0fc556d4b035f522ac7b
gwolanski <gwol@udel.edu>
2023-02-14 08:30:21 -0700

    Added ability to toggle drawing on and off

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+background: https://www.pexels.com/photo/multicolored-abstract-painting-1509534/
--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const divContainer = document.getElementById("gridContainer");
 
 let gridLength = 16;
+let color = "black";
+let coloringEnabled = false;
+let drawingStatus = document.querySelector("#drawingStatus");
+drawingStatus.textContent = "Drawing: Off";
 
 let slider = document.getElementById("myRange");
 let sliderValue = document.getElementById("myRange").value;
 let sliderOutput = document.getElementById("size");
 
-sliderOutput.innerHTML = `${slider.value} x ${slider.value}`;
+sliderOutput.innerHTML = `Grid Size: ${slider.value} x ${slider.value}`;
 slider.addEventListener("change", (e) => changeGrid(slider.value));
 slider.oninput = function() {
-        sliderOutput.innerHTML = `${this.value} x ${this.value}`;
+        sliderOutput.innerHTML = `Grid Size: ${this.value} x ${this.value}`;
 }
 
 function createGrid(size) {
@@ -21,11 +25,13 @@ function createGrid(size) {
     for (let i = 0; i < gridSize; i++) {
         let squareDiv = document.createElement("div");
         squareDiv.className = "gridItem";
-        squareDiv.addEventListener("mouseover", function () {
-            squareDiv.style.backgroundColor = "grey";
-        });
+        squareDiv.addEventListener("mouseover", colorDiv);
         divContainer.appendChild(squareDiv);
+
+        squareDiv.addEventListener("click", toggleDrawing);
+        
     }
+
 }
 
 function reloadGrid(size) {
@@ -39,36 +45,47 @@ function changeGrid(size) {
     reloadGrid(gridLength);
 }
 
-let resetButton = document.getElementById("reset");
+function colorDiv() {
+    if (coloringEnabled) {
+        if (color == 'rainbow') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        } else {
+            this.style.backgroundColor = "black";
+        }
+    } 
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function toggleDrawing() {
+    
+
+    if (coloringEnabled) {
+        coloringEnabled = false;
+        drawingStatus.innerHTML = "Drawing: Off";
+
+    } else {
+        coloringEnabled = true;
+        drawingStatus.innerHTML = "Drawing: On";
+    }
+}
+
+let resetButton = document.getElementById("resetbtn");
 resetButton.addEventListener('click', e => {
     let squareDiv = document.querySelectorAll(".gridItem");
     squareDiv.forEach(function(square) {
-        square.style.backgroundColor = "white"
+        square.style.backgroundColor = "seashell";
     });
 });
 
+let blackButton = document.getElementById("blackbtn");
+blackButton.addEventListener('click', e => {
+    color = "black";
+});
+
+
 createGrid(16);
 
-   
-let selectedDiv = document.querySelectorAll(".gridItem");
-//for (i=0; i < selectedDiv.length; i++) {
-  //  selectedDiv[i].addEventListener("mouseover", function () {
-    //    this.style.backgroundColor = "black";
-   // })
-// }
-
-
-
-    //create function that will clear/reset the grid
-   //     function clearGrid() {
-   //         resetButton.addEventListener('click', e => {
-   //             squareDivs.style.backgroundColor = "white";
-   //         })
-    //    }
-
-   //     clearGrid();
-   // }
-//}
-
-
-
+//create a function that stops/starts drawing when user clicks on one of the divs
